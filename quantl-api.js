e71d9607c715f6e5580262d9ffbaa962421bc298
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-10-26 14:10:17 -0700

    Url returns a valid API url. Doesn't graph results yet

--- quantl-api.js
+++ quantl-api.js
@@ -4,25 +4,26 @@
     window.AlphaSearchController = {
         init: () => {
             let searchButton = $("#search-button");
-            let searchParam = $("#radio-buttons radio:checked" + "DAILY");
             let searchTerm = $("#search-term");
-            let resultGraph = $(".graph")
+            let resultGraph = $(".graph");
             let url;
             // API Keys: ZJF5RAKQ4SUSZMR4
             searchButton.click(() => {
-                url = `https://www.alphavantage.co/query?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4&function=${searchParam.val()}&symbol=${searchTerm.val()}`;
+                let searchParam = $("input[name='radio-group']:checked");
+                let searchInterval = $('#FormControlSelect :selected');
+                url = `https://www.alphavantage.co/query?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4${searchParam.val()}${searchInterval.val()}&symbol=${searchTerm.val()}`;
                 $.getJSON(url).done(results => {
-                    results
-                    var data = {
-                        type: "scatter",
-                        mode: "lines",
-                        name: searchTerm.val(),
-                        x: unpack(rows, 'Date'),
-                        y: unpack(rows, 'AAPL.High'),
-                        line: {color: '#17BECF'}
-                    };
+                    let resultString = JSON.stringify(results);
+                    var data = [
+                      {
+                        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+                        y: [1, 3, 6],
+                        type: 'scatter'
+                      }
+                    ];
 
-//VERY VERY BROKEN
+                    Plotly.newPlot('graph', data);
+//VERY VERY BROKEN. Trying to Append/Add Plotly to the container graph
                     resultGraph.empty().append(
                     )
                     //  Plotly.newPlot('graph', data);
