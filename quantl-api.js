139cae532b2c240a64b17c9d759eb31b9009e6e1
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-10-26 19:36:56 -0700

    Added package-lock to enable Plotly and completed stock api

--- quantl-api.js
+++ quantl-api.js
@@ -13,21 +13,22 @@
                 let searchInterval = $('#FormControlSelect :selected');
                 url = `https://www.alphavantage.co/query?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4${searchParam.val()}${searchInterval.val()}&symbol=${searchTerm.val()}`;
                 $.getJSON(url).done(results => {
-                    let resultString = JSON.stringify(results);
-                    var data = [
-                      {
-                        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-                        y: [1, 3, 6],
-                        type: 'scatter'
-                      }
+                    const objName = Object.keys(results)[1];
+                    let xValues = Object.keys(results[objName]);
+                    let yValues = [];
+                    for (let i in xValues) {
+                        yValues.push(results[objName][xValues[i]]['1. open']);
+                    }
+                    let data = [
+                        {
+                            x: xValues,
+                            y: yValues,
+                            type: 'scatter'
+                        }
                     ];
-
-                    Plotly.newPlot('graph', data);
-//VERY VERY BROKEN. Trying to Append/Add Plotly to the container graph
-                    resultGraph.empty().append(
-                    )
-                    //  Plotly.newPlot('graph', data);
-                })
+                    Plotly.newPlot('graph', data)
+                    // resultGraph.empty().append();
+                });
             });
             searchTerm.bind("input", () => searchButton.prop("disabled", !searchTerm.val()));
         }
