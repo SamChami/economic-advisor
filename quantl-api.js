6b583e51ef8082915768a1b499a22e9357812b2b
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-10-26 20:01:19 -0700

    Added Crypto currency graph compatibility

--- quantl-api.js
+++ quantl-api.js
@@ -14,10 +14,11 @@
                 url = `https://www.alphavantage.co/query?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4${searchParam.val()}${searchInterval.val()}&symbol=${searchTerm.val()}`;
                 $.getJSON(url).done(results => {
                     const objName = Object.keys(results)[1];
+                    const propName = searchParam.val() === '&function=TIME_SERIES_' ? '4. close' : '4a. close (USD)';
                     let xValues = Object.keys(results[objName]);
                     let yValues = [];
                     for (let i in xValues) {
-                        yValues.push(results[objName][xValues[i]]['1. open']);
+                        yValues.push(results[objName][xValues[i]][propName]);
                     }
                     let data = [
                         {
