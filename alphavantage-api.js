d0918758996aba32e85469fa86761de484045b5f
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-11-02 00:37:17 -0700

    Fixed all Jenkins issues besides undefined Plotly

--- alphavantage-api.js
+++ alphavantage-api.js
@@ -5,15 +5,16 @@
         init: () => {
             let searchButton = $("#graph-button");
             let searchTerm = $("#search-term");
-            let resultGraph = $("#graph");
             let currencyButton = $("#currency-button");
             let currencyHolder = $("#currency-spot");
             let url;
-            // API Keys: ZJF5RAKQ4SUSZMR4
             searchButton.click(() => {
                 let searchParam = $("input[name='radio-group']:checked");
                 let searchInterval = $('#FormControlSelect :selected');
-                url = `https://www.alphavantage.co/query?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4${searchParam.val()}${searchInterval.val()}&symbol=${searchTerm.val()}`;
+                url = `https://www.alphavantage.co/query?outputsize=compact
+&apikey=ZJF5RAKQ4SUSZMR4${searchParam.val()}${searchInterval.val()}
+&symbol=${searchTerm.val()}`;
+
                 $.getJSON(url).done(results => {
                     const objName = Object.keys(results)[1];
                     const propName = searchParam.val() === '&function=TIME_SERIES_' ? '4. close' : '4a. close (USD)';
@@ -29,18 +30,19 @@
                             type: 'scatter'
                         }
                     ];
-
-                    Plotly.newPlot('graph', data)
-                    // resultGraph.empty().append();
+                    Plotly.newPlot('graph', data);
                 });
             });
             currencyButton.click(() => {
                 const dropdown1 = $('#currency_drop1 :selected');
                 const dropdown2 = $('#currency_drop2 :selected');
-                url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${dropdown1.val()}&to_currency=${dropdown2.val()}&apikey=ZJF5RAKQ4SUSZMR4`;
+                url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE
+&from_currency=${dropdown1.val()}&to_currency=${dropdown2.val()}&apikey=ZJF5RAKQ4SUSZMR4`;
                 $.getJSON(url).done(results => currencyHolder.empty().append(
-                  "1 " + results['Realtime Currency Exchange Rate']['2. From_Currency Name'] + " is equal to " + results['Realtime Currency Exchange Rate']['5. Exchange Rate'] + ' ' + results['Realtime Currency Exchange Rate']['4. To_Currency Name']
-                ))
+                    "1 " + results['Realtime Currency Exchange Rate']['2. From_Currency Name'] +
+                    " is equal to " + results['Realtime Currency Exchange Rate']['5. Exchange Rate'] +
+                    ' ' + results['Realtime Currency Exchange Rate']['4. To_Currency Name']
+                ));
             });
 
             searchTerm.bind("input", () => searchButton.prop("disabled", !searchTerm.val()));
