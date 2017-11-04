cfd987fbb0d20a7db56bf723b151a55121063f75
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-11-03 17:18:49 -0700

    All tests implemmented. Must now get tests to pass

--- test/alphavantage-api-test.js
+++ test/alphavantage-api-test.js
@@ -1,4 +1,4 @@
-describe("Alpha search example", () => {
+describe("Alpha search", () => {
     beforeEach(() => {
         fixture.setBase("test");
         fixture.load("search.fixture.html");
@@ -37,7 +37,7 @@ describe("Alpha search example", () => {
         });
     });
 
-    describe("API calls", () => {
+    describe("API calls - Stock Graphs", () => {
         var request;
 
         beforeEach(() => {
@@ -56,8 +56,9 @@ describe("Alpha search example", () => {
         });
 
         it("should trigger a Stock/Crypto search when the search button is clicked", () => {
-            expect(request.url).toBe(`https://www.alphavantage.co/query
-?outputsize=compact&apikey=ZJF5RAKQ4SUSZMR4&function=TIME_SERIES_DAILY&symbol=fb`);
+            expect(request.url).toBe(`https://www.alphavantage.co/query?outputsize=compact
+&apikey=ZJF5RAKQ4SUSZMR4&function=TIME_SERIES_DAILY
+&symbol=fb`);
         });
 
         it("should populate the graph container when search results arrive", () => {
@@ -66,9 +67,68 @@ describe("Alpha search example", () => {
             // Needs to check if graph is populated and with what
             request.respondWith({
                 status: 200,
+                responseText: JSON.stringify({
+                    "Meta Data": {},
+                    "Time Series (Daily)": {
+                        "2017-11-03": {
+                            "1. open": "179.2900",
+                            "2. high": "179.8600",
+                            "3. low": "176.7100",
+                            "4. close": "178.9200",
+                            "5. volume": "15270375"
+                        },
+                        "2017-11-02": {
+                            "1. open": "179.2900",
+                            "2. high": "179.8600",
+                            "3. low": "176.7100",
+                            "4. close": "178.9200",
+                            "5. volume": "15270375"
+                        }
+                    }
+                })
             }).toBe(true);
 
             expect($("#graph").children().length).toBe(1);
+        });
+    });
+
+    describe("API calls - Currency Exhange Rate", () => {
+        var request;
+
+        beforeEach(() => {
+            jasmine.Ajax.install();
+            $('#currency_drop1 :selected').val();
+            $('#currency_drop2 :selected').val();
+            $("#currency_amount").val();
+            $("#currency-button").click();
+
+            request = jasmine.Ajax.requests.mostRecent();
+        });
+
+        it("should trigger a Currency exchange search when the Convert button is clicked", () => {
+            expect(request.url).toBe(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE
+&from_currency=USD&to_currency=EUR&apikey=ZJF5RAKQ4SUSZMR4`);
+        });
+
+        it("should populate the container under the search the with an currency alert", () => {
+            request.respondWith({
+                status: 200,
+                responseText: {
+                    "Realtime Currency Exchange Rate": {
+                        "1. From_Currency Code": "USD",
+                        "2. From_Currency Name": "United States Dollar",
+                        "3. To_Currency Code": "EUR",
+                        "4. To_Currency Name": "Euro",
+                        "5. Exchange Rate": "0.86151200",
+                        "6. Last Refreshed": "2017-11-03 23:15:00",
+                        "7. Time Zone": "UTC"
+                    }
+                }
+            }).toBe(true);
+        });
+
+        afterEach(() => {
+            jasmine.Ajax.uninstall();
         });
     });
 });
